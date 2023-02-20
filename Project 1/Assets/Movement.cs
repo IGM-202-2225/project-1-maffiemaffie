b1bef4234b1c7d94bd90a16d998369d595be34db
Mafffie Cohen <mrc6113@rit.edu>
2023-02-19 21:07:56 -0500

    controls

--- Project 1/Assets/Movement.cs
+++ Project 1/Assets/Movement.cs
@@ -1,40 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     private Vector3 velocity = Vector3.zero;
     private Vector3 acceleration = Vector3.zero;
-    private Vector3 direction = Vector3.right;
+    private Vector3 direction = Vector3.zero;
 
-    private Vector3 Position
-    {
-        get
-        {
-            float _x = (transform.position.x + 10.0f) / 20.0f;
-            float _y = (transform.position.y + 5.0f) / 10.0f;
+    private Vector3 mousePos;
 
-            float scaledX = Mathf.LerpUnclamped(0, Screen.width, _x);
-            float scaledY = Mathf.LerpUnclamped(0, Screen.height, _y);
+    [SerializeField]
+    private Camera mainCam;
 
-            return new Vector3(scaledX, scaledY, transform.position.z);
-        }
-
-        set
-        {
-            float _x = value.x / Screen.width;
-            float _y = value.y / Screen.height;
-
-            float scaledX = Mathf.LerpUnclamped(-10.0f, 10.0f, _x);
-            float scaledY = Mathf.LerpUnclamped(-5.0f, 5.0f, _y);
-
-            transform.position = new Vector3(scaledX, scaledY, value.z);
-        }
-    }
+    [SerializeField]
+    private float MaxVelocity = 10f;
 
     [SerializeField]
     float strength = 10.0f;
+
+    [SerializeField]
+    float damping = 0.9f;
 
     // Start is called before the first frame update
     void Start()
@@ -45,14 +32,25 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
+        Vector3 position = transform.position;
+        Vector3 mouseWorldSpace = mainCam.ScreenToWorldPoint(mousePos);
+        Vector3 toCursor = mouseWorldSpace - transform.position;
+        direction = (direction + 0.5f * Time.deltaTime * toCursor).normalized;
 
-        Vector3 toCursor = mousePos - Position;
-        direction = Vector3.Normalize(toCursor);
-
-        acceleration = strength * toCursor.sqrMagnitude * direction;
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(toCursor.y, toCursor.x));
+        acceleration = strength * toCursor.magnitude * direction;
 
         velocity += acceleration * Time.deltaTime;
-        Position += velocity * Time.deltaTime;
+        if (velocity.sqrMagnitude > MaxVelocity * MaxVelocity) velocity = velocity.normalized * MaxVelocity;
+
+        velocity *= Mathf.Pow(damping, Time.deltaTime);
+
+        position += velocity * Time.deltaTime;
+        transform.position = new Vector3(position.x, position.y, 0);
+    }
+
+    void OnMove(InputValue value)
+    {
+        mousePos = value.Get<Vector2>();
     }
 }
