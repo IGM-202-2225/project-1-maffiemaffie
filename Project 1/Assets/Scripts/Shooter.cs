89aa5f01642cfb21f3aba087d6453c2cfddbf633
Mafffie Cohen <mrc6113@rit.edu>
2023-03-08 11:27:39 -0500

    big fire

--- Project 1/Assets/Scripts/Shooter.cs
+++ Project 1/Assets/Scripts/Shooter.cs
@@ -18,16 +18,30 @@ public class Shooter : MonoBehaviour
 
     private float sinceLastFire = 0;
 
+    private Queue<BigFireRound> bigFireQueue = new();
+
+    private struct BigFireRound
+    {
+        public int count;
+        public float offset;
+
+        public BigFireRound(int count, float offset)
+        {
+            this.count = count;
+            this.offset = offset;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        QueueBigFire();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Fire();
+        AdvanceTime();
     }
 
     void OnFire(InputValue fireValue)
@@ -35,17 +49,50 @@ public class Shooter : MonoBehaviour
         isFiring = fireValue.isPressed;
     }
 
-    void Fire()
+    void AdvanceTime()
     {
         sinceLastFire += Time.deltaTime;
-        if (!isFiring) return;
-        
         if (sinceLastFire > 1 / fireRate)
         {
-            sinceLastFire %= (1 / fireRate);
+            sinceLastFire = Time.deltaTime % fireRate;
+            Fire();
+            DoBigFire();
+        }
+    }
+
+    void Fire()
+    {
+        if (!isFiring) return;
+        
+        GameObject thisProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+        thisProjectile.GetComponent<BulletMove>().direction = transform.right;
+        collisionManager.GetComponent<Collision>().Projectiles.Add(thisProjectile);
+    }
+
+    void DoBigFire()
+    {
+        if (bigFireQueue.Count > 0)
+        {
+            BigFire(bigFireQueue.Dequeue());
+        }
+    }
+
+    void BigFire(BigFireRound round)
+    {
+        for (float i = round.offset; i < round.count + round.offset; i++)
+        {
+            float rotation = Mathf.PI / 4 * i;
             GameObject thisProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
-            thisProjectile.GetComponent<BulletMove>().direction = transform.right;
+            thisProjectile.GetComponent<BulletMove>().direction = new Vector3(Mathf.Cos(rotation), Mathf.Sin(rotation)).normalized * 0.5f;
             collisionManager.GetComponent<Collision>().Projectiles.Add(thisProjectile);
+        }
+    }
+
+    void QueueBigFire()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            bigFireQueue.Enqueue(new BigFireRound(8, 0.5f * (i % 2)));
         }
     }
 }
